cab8886efa5a6c6f1a306ddde351e8fa94855527
Jeffrey Buehler <jeffrey.d.buehler@hpe.com>
2017-01-11 12:47:55 -0700

    Internet Explorer simple

---
+++ internet_explorer.bat
@@ -0,0 +1,53 @@
+set /a ID=%random% %%40
+set list=A B C D
+
+IF "%ID%"=="0" set list=pass gas gaffe effective snuggle sleeve activate wire beard honor maid manner defend energy facility greeting jet afford health country unfortunate reserve flesh minimize confidence crossing pop knee confine cassette
+IF "%ID%"=="1" set list=deadly exaggerate eaux plane duty reference gold impound year continuation praise memorial fire planet flight dialogue novel scenario basis joystick eavesdrop insist division human body maid clerk divide bite father omission
+IF "%ID%"=="2" set list=discovery squeeze foster costume available write contact narrow dress clay ice insurance salvation publish offset cup abundant fuss sacrifice fantasy finish advance cemetery crevice greeting urine disk important consideration branch
+IF "%ID%"=="3" set list=halt boy sun reinforce firm inch opposition duke polish dignity recommendation goat glow cucumber stop dignity pollution bulb marble ceiling excuse council fan promote lace apathy first short circuit blow model
+IF "%ID%"=="4" set list=stamp can beginning canvas coach equip ideal try percent square umbrella green publisher reliable forge hypothesis jet robot mercy convict ivory cottage cotton bee mix resolution crown disappear soft friend
+IF "%ID%"=="5" set list=crevice talkative bother telephone slant competence elaborate forum winter employee exact basketball behave profit burn understanding state leadership displace bulletin justice appendix driver syndrome weak posture kidney soup apathy desert
+IF "%ID%"=="6" set list=coffin ivory approach spray walk coverage veteran adjust broccoli barrier rain pier screen chemistry unfair duke jump insure explode spectrum systematic cup scratch condition explosion kettle lock lodge disagreement impulse
+IF "%ID%"=="7" set list=dress decade divide abstract talkative low anniversary licence shout arrogant certain shop prevalence enfix awful deposit mutation tree sick soap handicap entitlement monarch temporary weave village compromise charge pass disorder
+IF "%ID%"=="8" set list=light conglomerate agile chorus beard lift similar stay predator systematic leftovers material mix planet hell bush install major fresh tumour palace retreat appetite desk wall normal code river dose word
+IF "%ID%"=="9" set list=lid building rabbit waist begin speculate car miss reflect change chase swim president island salt load mobile appointment slice rush thrust cabin flag nursery coffin hesitate crack stain freedom corpse
+IF "%ID%"=="10" set list=brick momentum cow sleeve tribe captain detector list straight version revive tycoon disco reality salt application transform rise harmony snow hostile shrink closed patrol anniversary meet glare user terrify appreciate
+IF "%ID%"=="11" set list=toast ego unpleasant sticky deserve derivative administration sympathetic opinion fun tread polite fit hall sow garage important penny gasp assault prospect century final loop sail suitcase stock behavior tank creed
+IF "%ID%"=="12" set list=wreck speculate declaration omission happen cow advertising leaflet letter seat society publish opposite mouse liability democratic infection drawer nationalism tumble environmental dressing introduction patrol wind opposed value sign throw capture
+IF "%ID%"=="13" set list=graduate tool continuous freeze impact hostage responsible tense poor ballot winner beg minister district pipe couple steak greet band gain cherry campaign glasses penetrate strap print ensure smell commitment convulsion
+IF "%ID%"=="14" set list=contradiction dream edition umbrella lean mass country mastermind ex doubt advance couple wander confront dismissal speech bloody minimum value mosquito pottery beautiful thin mine transmission attract mainstream bracket rib breast
+IF "%ID%"=="15" set list=tire decrease tendency install deck stunning adjust earwax grind multiply casualty provide sin sofa indulge morning slab debt unfortunate stroll broken affinity random representative flu memorial race palace average fashionable
+IF "%ID%"=="16" set list=interrupt express makeup minor hot scramble war enter blonde authorise burn back thick correspond venture genetic loan dirty connection personality promote sting truck spread oil burial instal photography win linen
+IF "%ID%"=="17" set list=piece metal television brag scenario refuse wrap treasurer colleague leg joke study feminist entertainment pudding decide progress row guess obligation undermine bundle chicken deer hear assumption applaud item censorship hospital
+IF "%ID%"=="18" set list=grind bake scale provincial executrix confine common cart movie inquiry feminine silver colon reign match union ladder rumor acute stable presidential elegant frown penetrate lung producer feel strikebreaker cooperation relaxation
+IF "%ID%"=="19" set list=tick ready digital regulation number presidential butterfly visit noble harmony friend wear out machinery triangle crusade bag pedestrian hover hostile clay piano splurge perform abnormal enemy battery rehabilitation handicap star lover
+IF "%ID%"=="20" set list=dry predator inhabitant bell licence patient decline fraction report census abuse ask improve wolf player fear explosion friendly mayor kit feast folklore seasonal mainstream glow affect ample install proof nationalist
+IF "%ID%"=="21" set list=gem slap sheep estimate insure mercy lay grand recommendation mine frown plot money reform chaos beach affair revenge prey manager traction quotation feast Venus expectation late liberty project social radio
+IF "%ID%"=="22" set list=system mature fish opera payment factor dead slap just extinct syndrome freckle dawn recover anger conception wrist recovery umbrella create outer candle stitch symbol memorandum cousin slot brag bury equip
+IF "%ID%"=="23" set list=tendency skeleton publish old relief update serve pipe supply hair researcher strong bargain example judge defeat penetrate patient theme trace grind vegetable tight item grateful character dare convulsion list spirit
+IF "%ID%"=="24" set list=tip vacuum charity aware disclose decade substitute fence minimize fountain tenant approach proportion demand capture demonstrate horizon reject premature fly patient announcement student burst second hike relative sow concentration miscarriage
+IF "%ID%"=="25" set list=deter service catch disaster commemorate orgy chalk activity flexible export quarter personality high shower tread joy temperature appreciate wolf loop inhibition wrong population marsh straighten convulsion rhythm depression kitchen bat
+IF "%ID%"=="26" set list=unaware provide option biology smash gold skip mist curve angel version difference spite sail justify treatment torch standard treat overlook pocket liberal give conglomerate test build waist sanctuary routine approve
+IF "%ID%"=="27" set list=trouser flush credit card road candidate reckless mail carrier neck critic charge fool mud rainbow firm provincial censorship neutral agile frown meaning lace swipe feeling husband minor courtesy radio salon net miracle
+IF "%ID%"=="28" set list=consensus replace acquit expression guitar temporary shark harmful magazine find apparatus jest surround youth dead river landscape expect possibility test kidnap pen eavesdrop steel asylum racism cinema rung clarify trainer
+IF "%ID%"=="29" set list=argument paradox sight safe steak torture application remark fuel limited band pace temptation merchant marriage bald cake improve fool around paint capital handicap sodium seasonal dressing precede heir demonstration embrace express
+IF "%ID%"=="30" set list=beneficiary bang count makeup twist key egg relative cassette transition deteriorate roar dismissal cooperation brother tile decrease kettle deficit gossip departure illustrate limited sequence lack empire dip pumpkin agriculture collection
+IF "%ID%"=="31" set list=action rifle voucher president display investigation disgrace nut pop snuggle generation pottery small kettle strap morning fear river pride concession official medal photography beam lonely judgment champion ash take hallway
+IF "%ID%"=="32" set list=bar orthodox replace pumpkin lead lifestyle power archive screen bad folklore prediction minimum hay strain refrigerator profound wrestle banner differ feign underline innocent weave swell think waist terms mud spring
+IF "%ID%"=="33" set list=doll lick random loss soap tread relate broken prevalence trick preparation worker avenue real sausage personality ethics dictionary finished elite ankle sailor chest build tick egg white sand wood wardrobe service
+IF "%ID%"=="34" set list=funeral line module cabin aloof basic penalty position employ spit punish receipt pound limit reputation stream linear leader background coalition character architecture employee beginning breeze sheep sin horse arm threshold
+IF "%ID%"=="35" set list=point perceive mean gradient instrument acute engagement ton houseplant trap conflict enjoy entertainment permission agent monopoly sleep palm breakdown pneumonia seize pack opposite image triangle indication porter private manufacture sticky
+IF "%ID%"=="36" set list=print attention raw promise kid corn summit revenge bark hammer effect compliance shape lock model anger association pavement preparation parameter sweater mirror situation commission brilliance inject project diplomatic work precision
+IF "%ID%"=="37" set list=obese bus inspector spin cane aunt knee disgrace conspiracy censorship allocation fee relieve shorts employee bowel point effect fascinate calendar miss correspond limit bucket panel organize affect see president guide
+IF "%ID%"=="38" set list=appeal acute constituency fee rotation weed feast sail scenario sharp alive beat shelf bed attract affect tower compromise margin chief exaggerate flu run economy kneel era tape star paper equinox
+IF "%ID%"=="39" set list=apparatus due landowner chaos fall retire swallow whip established world mile current officer development key push collection pilot hurl stand gesture banquet aid cell pray function spray pavement old age word
+
+
+# Every 5 seconds, open up a new tab with the word selected from the list.
+(for %%a in (%list%) do (
+	start /d "C:\Program Files\Internet Explorer" IEXPLORE.EXE http://www.bing.com/search?q=%%a
+	timeout 5
+))
+
+# Pull up a tab for the Daily Points
+start microsoft-edge:https://account.microsoft.com/rewards/dashboard?refd=www.bing.com
